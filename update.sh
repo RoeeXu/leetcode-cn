fe23e24bc0b1770a78b67c91c4114b878188ca87
roeexu <roeexu@gmail.com>
2019-11-14 11:14:52 +0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+#####################################################
+# Copyright (c) 2019 Roee Xu. All Rights Reserved
+#####################################################
+# File:    update.sh
+# Author:  roeexu
+# Date:    2019/11/14 11:13:35
+# Brief:
+#####################################################
+
+TIME=`date +'%Y-%m-%d %H:%M:%S'`
+git add *; git commit -m "$TIME"; git push
+
+# vim: set expandtab ts=4 sw=4 sts=4 tw=100
+
