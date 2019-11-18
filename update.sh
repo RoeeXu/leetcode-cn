f4c5e7f891ae6c82114993515b1b6c82563a29a3
roeexu <roeexu@gmail.com>
2019-11-18 22:34:50 +0800

    Modified update script

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@
 # Brief:
 #####################################################
 
-git pull
 TIME=`date +'%Y-%m-%d %H:%M:%S'`
+git pull
 git add *; git commit -m "$TIME"; git push
 
 # vim: set expandtab ts=4 sw=4 sts=4 tw=100
